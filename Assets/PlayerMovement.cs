d293e252c16ec1d7b2d2ee6248a4bc1a39ff3634
Jaida Plair <jaiplair@gmail.com>
2024-04-10 09:51:45 -0400

    FACE

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,33 +5,68 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed = 8f;
-    [SerializeField] float forceX = 1f;
-    [SerializeField] float forceY = 10f;
-    SpriteRenderer spriteRenderer;
-    //Rigidbody2D rb;
+    [SerializeField] float jumpForce = 10f;
+    [SerializeField] float rotationSpeed = 500f; // Adjust the rotation speed as needed
+    bool isGrounded = false;
+    Rigidbody2D rb;
 
-    public float jumpForce = 10f;
-    private Rigidbody2D rb;
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-       
-        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.right * speed* Time.deltaTime);
+        // Move the player horizontally
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Jump when Space key is pressed and the player is grounded
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            //rb.AddForce(forceY * Vector2.up);//applying upward force
-            
-            //spriteRenderer.flipX = false;
+            // Rotate the player
+            StartCoroutine(RotatePlayer(90f));
+        }
+    }
 
+    IEnumerator RotatePlayer(float targetAngle)
+    {
+        float startAngle = transform.eulerAngles.z;
+        float t = 0f;
+        while (t < 1f)
+        {
+            t += Time.deltaTime * rotationSpeed;
+            float angle = Mathf.Lerp(startAngle, targetAngle, t);
+            transform.eulerAngles = new Vector3(0, 0, angle);
+            yield return null;
+        }
+        transform.eulerAngles = new Vector3(0, 0, targetAngle);
+        yield return null;
+    }
+
+    // Check if the player is grounded
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            // Destroy the object collided with if it has a hit tag
+            if (collision.gameObject.CompareTag("Hit"))
+            {
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
         }
     }
 }
+
