87d4e3133fa8207f6bcb489093c3f7c4ba8b4a94
Jaida Plair <jaiplair@gmail.com>
2024-04-26 16:47:16 -0400

    map

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -7,7 +7,7 @@ using UnityEngine.SocialPlatforms.Impl;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] TMP_Text ScoreUiText;
-    public int score = 0;
+    public int score = 1;
     [SerializeField] GameObject fades;
     // Start is called before the first frame update
     void Start()
